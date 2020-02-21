af76c4ba9ac633373db690debed88dc73edc21c2
Ing. Fhillip Castillo <gerson.fhillip@gmail.com>
2020-02-21 00:47:05 -0400

    WIP - automated saved

--- check-git.sh
+++ check-git.sh
@@ -1,5 +1,5 @@
 VN=$(git describe --abbrev=7 HEAD 2>/dev/null)
-LASTCOMMIT=""
+LASTCOMMIT="$(git log -1 --oneline)"
 git update-index -q --refresh
 CHANGED="$(git diff-index --name-only HEAD --)"
 if [ -n "$CHANGED" ]; then
@@ -8,7 +8,8 @@ if [ -n "$CHANGED" ]; then
     COMMIT=$(git commit -am"WIP - automated saved")
     echo "commit done"
     LASTCOMMIT=COMMIT
-    echo COMMIT
+    echo $COMMIT
 else
   echo "doesn't have changes"
+  echo $LASTCOMMIT
 fi
