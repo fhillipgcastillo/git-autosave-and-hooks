7d543b6055d2be21f40c40489ed2d31b909784d1
Ing. Fhillip Castillo <gerson.fhillip@gmail.com>
2020-02-21 00:34:47 -0400

    WIP - automated saved

--- check-git.sh
+++ check-git.sh
@@ -1,7 +1,14 @@
 VN=$(git describe --abbrev=7 HEAD 2>/dev/null)
-
+LASTCOMMIT=""
 git update-index -q --refresh
-CHANGED=$(git diff-index --name-only HEAD --)
+CHANGED="$(git diff-index --name-only HEAD --)"
 if [ -n "$CHANGED" ]; then
     VN="$VN-mod"
+    echo "have changes"
+    COMMIT=$(git commit -am"WIP - automated saved")
+    echo "commit done"
+    LASTCOMMIT=COMMIT
+    echo COMMIT
+else
+  echo "doesn't have changes"
 fi
