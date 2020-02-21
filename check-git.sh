80e0c03f5c19ff63fa47e78c266bfa44d753729d
Ing. Fhillip Castillo <gerson.fhillip@gmail.com>
2020-02-21 10:02:28 -0400

    add a clean code

--- check-git.sh
+++ check-git.sh
@@ -1,15 +1,47 @@
-VN=$(git describe --abbrev=7 HEAD 2>/dev/null)
-LASTCOMMIT="$(git log -1 --oneline)"
-git update-index -q --refresh
-CHANGED="$(git diff-index --name-only HEAD --)"
-if [ -n "$CHANGED" ]; then
-    VN="$VN-mod"
-    echo "have changes"
-    COMMIT=$(git commit -am"WIP - automated saved")
-    echo "commit done"
-    LASTCOMMIT=COMMIT
-    echo $COMMIT
-else
-  echo "doesn't have changes"
-  echo $LASTCOMMIT
-fi
+#!/bin/sh
+
+commit_wip_changes(){
+  local VN="$VN-mod"
+  local COMMIT=$(git commit -am"WIP - automated saved")
+  echo "commit done $COMMIT"
+  lastcommit=$(git log -1 --oneline)
+  return $lastcommit
+}
+
+main (){
+  # while [ true ]
+  # do
+    local LASTCOMMIT="$(git log -1 --oneline)"
+    echo "$LASTCOMMIT"
+    havechanges= $(verifychanges $LASTCOMMIT)
+    local hChanges=1
+    echo "have changes $havechanges eq $hChanges"
+
+    if [$havechanges -ne $hChanges ];
+      then
+        echo "found changes"
+        # LASTCOMMIT=$(commit_wip_changes)
+        # echo "last commit $LASTCOMMIT"
+      else 
+        echo "No new changes found"
+    fi
+
+    echo "sleeping 5s"
+    SLEEP 3s
+  # done
+}
+
+verifychanges () {
+  # $1 => lastcommit cached
+  local VN=$(git describe --abbrev=7 HEAD 2>/dev/null)
+  local lastcommit="$(git log -1 --oneline)"
+  git update-index -q --refresh
+  local CHANGED="$(git diff-index --name-only HEAD --)"
+  if [ -n "$CHANGED" ]; then
+    return "1"
+  else
+    return "0"
+  fi
+}
+
+main
